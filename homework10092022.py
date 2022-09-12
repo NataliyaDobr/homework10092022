9c28d96e3ce51202f9fafb74f1b421e2da770b63
NataliyaDobr <n.m.suvorova@mail.ru>
2022-09-12 10:28:41 +0300

    task2

--- homework10092022.py
+++ homework10092022.py
@@ -10,10 +10,22 @@
 # 2. Напишите программу, которая найдёт произведение пар чисел списка. Парой считаем первый и последний элемент, второй и предпоследний и т.д. Пример:
 # - [2, 3, 4, 5, 6] => [12, 15, 16];
 # - [2, 3, 5, 6] => [12, 15]
-some_list = [int(input('Введите строку ')) for _ in range(int(input('введите число ')))]
+# some_list = [int(input('Введите строку ')) for _ in range(int(input('введите число ')))]
+# print(some_list)
+# new_list = some_list[::-1]
+# print(new_list)
+# for i in range(round(len(some_list)/2)):
+#     result = some_list[i]*new_list[i]
+#     print(result)
+
+# 3. Задайте список из вещественных чисел. Напишите программу, которая найдёт разницу между максимальным и минимальным значением дробной части элементов.
+# Пример: - [1.1, 1.2, 3.1, 5, 10.01] => 0.19
+fract_part = []
+some_list = [float(input('Введите числа ')) for _ in range(int(input('введите число ')))]
 print(some_list)
-new_list = some_list[::-1]
-print(new_list)
-for i in range(round(len(some_list)/2)):
-    result = some_list[i]*new_list[i]
-    print(result)
+for i in range(len(some_list)):
+    fract_part.append(some_list[i]-int(some_list[i]))
+print(fract_part)
+print(max(fract_part)-min(fract_part))
+
+
