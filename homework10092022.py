8d80aac5789aa30aa10dc32ffe80695c13c1755d
NataliyaDobr <n.m.suvorova@mail.ru>
2022-09-12 15:54:01 +0300

    task5

--- homework10092022.py
+++ homework10092022.py
@@ -32,15 +32,30 @@
 # - 45 -> 101101
 # - 3 -> 11
 # - 2 -> 10
-new_digit=[]
-digit = int(input('Введите число '))
-while digit > 1:
-    rem = digit%2
-    #print(rem)
-    new_digit.append(rem)
-    digit = digit//2
-new_digit.append(digit)
-print(" ".join(map(str, new_digit[::-1])))
+# new_digit=[]
+# digit = int(input('Введите число '))
+# while digit > 1:
+#     rem = digit%2
+#     #print(rem)
+#     new_digit.append(rem)
+#     digit = digit//2
+# new_digit.append(digit)
+# print(" ".join(map(str, new_digit[::-1])))
+
+# 5. Задайте число. Составьте список чисел Фибоначчи, в том числе для отрицательных индексов.Пример:
+# - для k = 8 список будет выглядеть так: [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21]
+n = int(input('Введите число '))
+m=n
+fib = [0,1]
+while len(fib) < n+1:
+    fib.append(fib[-2]+fib[-1])
+print(fib)
+neg_fib = [1,-1]
+while len(neg_fib) < m:
+    neg_fib.append(neg_fib[-2]-neg_fib[-1])
+print(neg_fib)
+print(" ".join(map(str, neg_fib[::-1]))," ".join(map(str, fib)))
+
 
 
 
